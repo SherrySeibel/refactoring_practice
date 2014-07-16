3a25c2ed518af63704fa42b1a5729a8787611090
Sherry Seibel <sherry.seibel@gmail.com>
2014-07-16 18:36:18 -0400

    Merge two methods

--- hotel/hotel_finder.rb
+++ hotel/hotel_finder.rb
@@ -5,18 +5,13 @@ class HotelFinder
     @file_name = file_name
   end
 
-  def find_hotels
-    display_hotel_name
-  end
-
-  private
-
-  def display_hotel_name
+  def display_hotel_names
     CSV.foreach(@file_name, headers: true) do |row|
       puts row["Hotel"]
     end
   end
+
 end
 
 hotel_finder = HotelFinder.new("hotels.csv")
-hotel_finder.find_hotel
+hotel_finder.display_hotel_names
