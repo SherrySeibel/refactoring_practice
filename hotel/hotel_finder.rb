32b5d24a3c6ed85ec042796de8a67956be23a452
Sherry Seibel <sherry.seibel@gmail.com>
2014-07-16 17:51:30 -0400

    Fix errors

--- hotel/hotel_finder.rb
+++ hotel/hotel_finder.rb
@@ -1,23 +1,32 @@
-# require "csv"
+require "csv"
 # require "pp"
 
 class HotelFinder
   def initialize(hotel_database)
     @hotel_database = hotel_database
+    @hotels = {}
   end
 
   def find_hotel
     display_hotel_name
+    # hotel_query
   end
 
   private
 
   def display_hotel_name
-    hotel_search = @hotel_database.foreach("hotels.csv", headers: true) do |row|
-      puts row[0]
+    hotel_list = CSV.foreach(@hotel_database, headers: true) do |row|
+      puts row["Hotel"]
     end
   end
+
+  # def hotel_query
+  #   print "What property? > "
+  #   user_query = gets.chomp
+  #   @hotels.map { |hotel, info| hotel }
+  #   # puts user_query
+  # end
 end
 
-hotel_finder = HotelFinder.new(CSV)
+hotel_finder = HotelFinder.new("hotels.csv")
 hotel_finder.find_hotel
