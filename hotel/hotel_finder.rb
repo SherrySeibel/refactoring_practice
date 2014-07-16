f9499618d2ed3299717f6ce6f03836e735f3c290
Sherry Seibel <sherry.seibel@gmail.com>
2014-07-16 18:22:59 -0400

    List the hotels saved in database

--- hotel/hotel_finder.rb
+++ hotel/hotel_finder.rb
@@ -3,7 +3,6 @@ require "csv"
 class HotelFinder
   def initialize(hotel_database)
     @hotel_database = hotel_database
-    @hotels = {}
   end
 
   def find_hotel
@@ -14,11 +13,9 @@ class HotelFinder
 
   def display_hotel_name
     hotel_list = CSV.foreach(@hotel_database, headers: true) do |row|
-      @hotels[row.fields[0]] = Hash[row.headers[1..-1]]
+      puts row["Hotel"]
     end
   end
-
-  puts @hotels
 end
 
 hotel_finder = HotelFinder.new("hotels.csv")
