e4d7ed67590ec58d5ee5c7f7b4f3850e24188024
Sherry Seibel <sherry.seibel@gmail.com>
2014-09-22 17:47:58 -0400

    Extracted PLAYER_VS_AI_RULES to new class

--- rock_paper_scissors/game_rules.rb
+++ rock_paper_scissors/game_rules.rb
@@ -1,46 +1,34 @@
-require "./ai"
-require "./player"
+require "./player_vs_ai"
 
 class GameRules
   def initialize
     @player = Player.new
     @ai = Ai.new
+    @player_vs_ai = PlayerVsAi.new
   end
-
-  PLAYER_VS_AI_RULES = {
-    "R" => { "R" => "Player ties AI", "P" => "AI wins", "S" => "Player wins" },
-    "P" => { "R" => "Player wins", "P" => "Player ties AI", "S" => "AI wins" },
-    "S" => { "R" => "AI wins", "P" => "Player wins", "S" => "Player ties AI" }
-  }
 
   def implement_rules
     game_loop
-    determine_winner
-    randomized_ai_move
   end
 
   private
 
   def game_loop
     loop do
-      display_prompt
-      player_turn = @player.turn
+      player_turn = display_prompt
 
       if player_turn == "Q"
         break
       else
         ai_turn = randomized_ai_move
-        determine_winner(player_turn, ai_turn)
+        @player_vs_ai.determine_winner(player_turn, ai_turn)
       end
     end
   end
 
   def display_prompt
     print "Your move? (R/P/S or q to quit) > "
-  end
-
-  def determine_winner(player_turn, ai_turn)
-    puts PLAYER_VS_AI_RULES[player_turn][ai_turn]
+    @player.turn
   end
 
   def randomized_ai_move
